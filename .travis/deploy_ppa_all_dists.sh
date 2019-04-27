e418b3d7200ab1f55cb98507891d2951efda1dbc
Vincent Rivière <vincent.riviere@freesbee.fr>
2019-04-27 09:44:23 +0200

    Add PPA for Ubuntu 19.04 Disco Dingo
    https://launchpad.net/~vriviere/+archive/ubuntu/ppa/?field.series_filter=disco

--- .travis/deploy_ppa_all_dists.sh
+++ .travis/deploy_ppa_all_dists.sh
@@ -16,6 +16,7 @@ DISTS+=" trusty"
 DISTS+=" xenial"
 DISTS+=" bionic"
 DISTS+=" cosmic"
+DISTS+=" disco"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

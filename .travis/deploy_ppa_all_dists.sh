3811ce46d1d4fa9095d5c1fb50e2e0cf8a35da87
Vincent Rivière <vincent.riviere@freesbee.fr>
2020-04-25 10:55:20 +0200

    Build binaries for Ubuntu 20.04 Focal Fossa

--- .travis/deploy_ppa_all_dists.sh
+++ .travis/deploy_ppa_all_dists.sh
@@ -16,6 +16,7 @@ DISTS+=" xenial"
 DISTS+=" bionic"
 DISTS+=" disco"
 DISTS+=" eoan"
+DISTS+=" focal"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

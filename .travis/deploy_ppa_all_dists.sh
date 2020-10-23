845b636f493caaacd964a8cb787870d2cbc2c66c
Vincent Rivière <vincent.riviere@freesbee.fr>
2020-10-23 21:42:32 +0200

    Build binaries for Ubuntu 20.10 Groovy Gorilla

--- .travis/deploy_ppa_all_dists.sh
+++ .travis/deploy_ppa_all_dists.sh
@@ -15,6 +15,7 @@ DISTS=
 DISTS+=" xenial"
 DISTS+=" bionic"
 DISTS+=" focal"
+DISTS+=" groovy"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

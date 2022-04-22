dbc4aeffb1457b24dc18bb88abccdda382d4b9e3
Vincent Rivière <vincent.riviere@freesbee.fr>
2022-04-22 22:03:03 +0200

    Build binaries for Ubuntu 22.04 Jammy Jellyfish

--- .travis/deploy_ppa_all_dists.sh
+++ .travis/deploy_ppa_all_dists.sh
@@ -15,6 +15,7 @@ DISTS=
 DISTS+=" bionic"
 DISTS+=" focal"
 DISTS+=" impish"
+DISTS+=" jammy"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

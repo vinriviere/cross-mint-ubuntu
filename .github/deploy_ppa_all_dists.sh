d6b5244ad3f7cce1a92212c9e68b29ee9fd4fa2a
Vincent Rivière <vincent.riviere@freesbee.fr>
2023-10-13 14:39:20 +0200

    Build binaries for Ubuntu 23.10 Mantic Minotaur

--- .github/deploy_ppa_all_dists.sh
+++ .github/deploy_ppa_all_dists.sh
@@ -16,6 +16,7 @@ DISTS+=" focal"
 DISTS+=" jammy"
 DISTS+=" kinetic"
 DISTS+=" lunar"
+DISTS+=" mantic"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

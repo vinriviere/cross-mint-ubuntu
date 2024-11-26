acf36278548b93c4b10d304d185a8dfb901aa798
Vincent Rivière <vincent.riviere@freesbee.fr>
2024-11-27 00:00:00 +0100

    Build binaries for Ubuntu 24.10 Oracular Oriole

--- .github/deploy_ppa_all_dists.sh
+++ .github/deploy_ppa_all_dists.sh
@@ -15,6 +15,7 @@ DISTS=
 DISTS+=" focal"
 DISTS+=" jammy"
 DISTS+=" noble"
+DISTS+=" oracular"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)

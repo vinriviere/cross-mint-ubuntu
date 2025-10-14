4dced601e11400b4e05d907197b506c49cb30358
Vincent Rivière <Vincent.Riviere@univ-paris1.fr>
2025-10-14 18:15:23 +0200

    Build binaries for Ubuntu 25.10 Questing Quokka

--- .github/deploy_ppa_all_dists.sh
+++ .github/deploy_ppa_all_dists.sh
@@ -15,6 +15,7 @@ DISTS=
 DISTS+=" jammy"
 DISTS+=" noble"
 DISTS+=" plucky"
+DISTS+=" questing"
 
 # Use timestamp as package suffix to generate a unique version number
 SUFFIX=ppa$(date -u +%Y%m%d%H%M%S)
